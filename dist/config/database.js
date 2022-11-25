a8dac18b68e90e5a7bb83bcfbee26e4dbbcf4d28
Ibrahim Hadi Abdali <ibrahimha@outlook.com>
2022-11-25 19:22:24 +0300

    adding herouku config

--- dist/config/database.js
+++ dist/config/database.js
@@ -4,11 +4,11 @@ exports.default = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', '172.17.0.3'),
+            host: env('DATABASE_HOST', 'ec2-54-75-184-144.eu-west-1.compute.amazonaws.com'),
             port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi-butlah'),
-            user: env('DATABASE_USERNAME', 'postgres'),
-            password: env('DATABASE_PASSWORD', 'postgres'),
+            database: env('DATABASE_NAME', 'dfue6nci1b9491'),
+            user: env('DATABASE_USERNAME', 'lqpjztpxlagnmz'),
+            password: env('DATABASE_PASSWORD', '5c7c21fb44cf6f9ac09efa883bb60c6681dc0a9c85cd697f00c8852ff04c2b93'),
             ssl: env.bool('DATABASE_SSL', false),
         },
     },
